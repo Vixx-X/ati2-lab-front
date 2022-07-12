ce9779815758df4f7df8b442aa758411ba747795
Gabriela <gabrielaustariz@gmail.com>
2022-07-11 21:21:40 -0400

    feat(modals): added modal to business section

---
+++ src/components/layout/modals/GeneralModal.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
+export default function Modal({ button, open, handleClose, CancelButton, AcceptButton, children }: any) {
+
+    const styles = {
+        '& .MuiPaper-root': {
+            maxWidth: '900px'
+        },
+    }
+
+    return (
+        <div>
+            {button}
+            <Dialog open={open} onClose={handleClose} sx={styles}>
+                <DialogTitle>Subscribe</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To subscribe to this website, please enter your email address here. We
+                        will send updates occasionally.
+                    </DialogContentText>
+                    {children}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Subscribe</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
--- src/pages/business/index.tsx
+++ src/pages/business/index.tsx
@@ -3,6 +3,20 @@ import type { NextPage } from 'next';
 import MainContainer from '@components/layout/MainContainer';
 
 import MiTable from '../../components/table/MiTable';
+import DomainAddIcon from '@mui/icons-material/DomainAdd';
+import IconButton from '@mui/material/IconButton';
+import Button from '@components/layout/Button';
+import FormDialog from '@components/layout/modals/GeneralModal';
+import { useState } from 'react';
+import { FormikValues } from 'formik';
+import Form from '@components/forms/Form';
+import { Field } from 'formik';
+import ErrorMsg from '@components/forms/ErrorMsg';
+import PassField from '@components/forms/PassField';
+import Loader from '@components/Loader';
+import { SOCIAL } from '@components/data/SocialNetworks';
+import Select from '@components/forms/Select';
+import AddIcon from '@mui/icons-material/Add';
 
 const businessData = [
   {
@@ -46,11 +60,326 @@ const businessData = [
   },
 ];
 
+const BusinessButton = ({ onclick }: any) => {
+
+  return (
+    <Button endIcon={<DomainAddIcon />} onclick={onclick}>
+      Añadir Empresa
+    </Button>
+  )
+}
+
+
 const Business: NextPage = () => {
+
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const [loading, setLoading] = useState(false);
+
+  const initValues = {
+    password: '',
+    username: '',
+  };
+  // as SigninForm
+
+  const handleSubmit = async (values: FormikValues, { setStatus }: any) => {
+    setLoading(true);
+    try {
+      // await login(values.username, values.password);
+      setStatus({});
+    } catch (exception: any) {
+      setStatus(exception.data);
+      setLoading(false);
+    }
+  };
+
+
   return (
     <MainContainer>
+      <FormDialog 
+        button={<BusinessButton onclick={handleClickOpen} />}
+        open={open}
+        handleClose={handleClose}
+        CancelButton={""}
+        AcceptButton={""}>
+        <Form initialValues={initValues} onSubmit={handleSubmit}>
+          <>
+            <div className="pt-4">
+              <div className="flex gap-x-8">
+                <div className="mb-4 text-sm basis-2/4">
+                  <label htmlFor="name">
+                    Nombre
+                  </label>
+                  <Field
+                    label="Nombre de usuario"
+                    name="name"
+                    id="name"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Nombre de la empresa"
+                  />
+                  <ErrorMsg name="name" />
+                </div>
+                <div className="mb-4 text-sm basis-2/4">
+                  <label
+                    htmlFor="id_number"
+                  >
+                    Numero de identificación tributaria
+                  </label>
+                  <Field
+                    label="Numero de identificación tributaria"
+                    name="id_number"
+                    id="id_number"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Numero de identificación tributaria"
+                  />
+                  <ErrorMsg name="id_number" />
+                </div>
+              </div>
+              <div className="flex gap-x-8">
+                <div className="mb-4 text-sm basis-2/4">
+                  <label
+                    htmlFor="phone_number"
+                  >
+                    Teléfono
+                  </label>
+                  <Field
+                    label="Teléfono"
+                    name="phone_number"
+                    id="phone_numner"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Teléfono"
+                  />
+                  <ErrorMsg name="phone_number" />
+                </div>
+                <div className="mb-4 text-sm basis-2/4">
+                  <label
+                    htmlFor="email"
+                  >
+                    E-mail
+                  </label>
+                  <Field
+                    label="E-mail"
+                    name="email"
+                    id="email"
+                    type="email"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1 text-sm"
+                    placeholder="E-mail"
+                  />
+                  <ErrorMsg name="email" />
+                </div>
+              </div>
+              <div className="flex gap-x-8">
+                <div className="mb-4 text-sm basis-2/4">
+                  <label
+                    htmlFor="website"
+                  >
+                    Sitio Web
+                  </label>
+                  <Field
+                    label="Sitio web"
+                    name="website"
+                    id="website"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Sitio Web"
+                  />
+                  <ErrorMsg name="website" />
+                </div>
+                <div className="mb-4 text-sm basis-2/4">
+                  <label
+                    htmlFor="whatsapp"
+                  >
+                    Whatsapp
+                  </label>
+                  <Field
+                    label="Whatsapp"
+                    name="whatsapp"
+                    id="whatsapp"
+                    type="whatsapp"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Whatsapp"
+                  />
+                  <ErrorMsg name="whatsapp" />
+                </div>
+              </div>
+              <div className="flex gap-x-8">
+                <div className="mb-4 text-sm basis-2/4">
+                  <label htmlFor="request_service">
+                    Servicio solicitado
+                  </label>
+                  <Field
+                    label="Servicio solicitado"
+                    name="request_service"
+                    id="request_service"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Servicio solicitado"
+                  />
+                  <ErrorMsg name="request_service" />
+                </div>
+                <div className="mb-4 text-sm basis-2/4">
+                  <label
+                    htmlFor="offert_service"
+                  >
+                    Servicios que ofrezco
+                  </label>
+                  <Field
+                    label="Servicios que ofrezco"
+                    name="offert_service"
+                    id="offert_service"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Servicios que ofrezco"
+                  />
+                  <ErrorMsg name="offert_service" />
+                </div>
+              </div>
+              <div className="flex gap-x-8">
+                <div className="mb-4 text-sm basis-2/4">
+                  <label htmlFor="pref_course">
+                    Cursos de interés
+                  </label>
+                  <Field
+                    label="Cursos de interés"
+                    name="pref_course"
+                    id="pref_course"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Cursos de interés"
+                  />
+                  <ErrorMsg name="pref_course" />
+                </div>
+                <div className="mb-4 text-sm basis-2/4">
+                  <label
+                    htmlFor="notification_frecuency"
+                  >
+                    Frecuencia de Notificaciones
+                  </label>
+                  <Field
+                    label="Frecuencia de Notificaciones"
+                    name="notification_frecuency"
+                    id="notification_frecuency"
+                    className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                    placeholder="Frecuencia de Notificaciones"
+                  />
+                  <ErrorMsg name="notification_frecuency" />
+                </div>
+              </div>
+              <div>
+                <div className="flex gap-x-8">
+                  <div className="mb-1 text-sm basis-1/3">
+                    <label htmlFor="country">
+                      País
+                    </label>
+                    <Field
+                      label="País"
+                      name="country"
+                      id="country"
+                      className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                      placeholder="País"
+                    />
+                    <ErrorMsg name="country" />
+                  </div>
+                  <div className="mb-1 text-sm basis-1/3">
+                    <label
+                      htmlFor="city"
+                    >
+                      Ciudad
+                    </label>
+                    <Field
+                      label="Ciudad"
+                      name="city"
+                      id="city"
+                      className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                      placeholder="Ciudad"
+                    />
+                    <ErrorMsg name="city" />
+                  </div>
+                  <div className="mb-1 text-sm basis-1/3">
+                    <label
+                      htmlFor="state"
+                    >
+                      Estado
+                    </label>
+                    <Field
+                      label="Estado"
+                      name="state"
+                      id="state"
+                      className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                      placeholder="Estado"
+                    />
+                    <ErrorMsg name="state" />
+                  </div>
+                </div>
+                <div className="flex gap-x-8">
+                  <div className="mb-4 text-sm basis-2/4">
+                    <label htmlFor="line1">
+                      Línea 1
+                    </label>
+                    <Field
+                      label="Línea 1"
+                      name="line1"
+                      id="line1"
+                      className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                      placeholder="Línea 1"
+                    />
+                    <ErrorMsg name="line1" />
+                  </div>
+                  <div className="mb-4 text-sm basis-2/4">
+                    <label
+                      htmlFor="line2"
+                    >
+                      Línea 2
+                    </label>
+                    <Field
+                      label="Línea 2"
+                      name="line2"
+                      id="line2"
+                      className="rounded py-2 px-2 text-gray-600 w-full mt-1"
+                      placeholder="Línea 2"
+                    />
+                    <ErrorMsg name="line2" />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <label htmlFor="social_newtworks">
+                  Redes sociales
+                </label>
+                <div className="flex gap-x-16 justify-between">
+                  <div className="basis-4/5 gap-x-4 text-sm flex">
+                    <div className="basis-1/5">
+                    <Select choices={SOCIAL} placeholder='Red Social' />
+                    </div>
+                    <Field
+                      label=""
+                      name="social_network"
+                      id="social_network"
+                      className="rounded py-2 px-2 text-gray-600 mt-1 basis-4/5"
+                      placeholder=""
+                    />
+                  </div>
+                  <div className="basis-1/5">
+                    <Button endIcon={<AddIcon/>}>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-6">
+              </div>
+            </div>
+            <ErrorMsg name="detail" />
+            {loading && <Loader />}
+          </>
+        </Form>
+      </FormDialog>
       <MiTable rows={businessData}></MiTable>
-    </MainContainer>
+    </MainContainer >
   );
 };
 
