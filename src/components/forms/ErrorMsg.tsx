baa4ba86129176c6f64cb4dfac8ee24b89a38900
vixx_ <vittorio.adesso@gmail.com>
2022-07-17 08:18:33 -0400

    feat: recursive error

--- src/components/forms/ErrorMsg.tsx
+++ src/components/forms/ErrorMsg.tsx
@@ -1,29 +1,27 @@
+import { useMemo } from 'react';
+
+import recursiveGetter from '@utils/recursiveGetter';
+
 import { ErrorMessage, ErrorMessageProps, useFormikContext } from 'formik';
-import { useState } from 'react';
+
 export const ErrorMsg = ({ name, ...props }: ErrorMessageProps) => {
   const { status } = useFormikContext();
-  const statusData = status?.detail; 
-  // console.log("STATUS EN ERROR MESAGGE", status);
-  const nestedArray = name.split('.');
-  const [nested, setNested] = useState<boolean>((nestedArray[1])? true : false) 
-  let nameF = name.replaceAll(".", "?.");
-  // const errorMessage = eval(`statusData?.${nameF}`)
-  // console.log("aaaa", `statusData?.${nameF}`, errorMessage)
+  const errorMessage = useMemo(() => {
+    console.log(name, status);
+    return recursiveGetter(status, name, null);
+  }, [status, name]);
 
   const Message = ({ error }: any) => {
     return (
       <div className="bg-red-600 border border-red-700 w-full p-3 my-3 py-2 rounded-lg text-sm font-normal">
-        {console.log("a e", error)}
-        {error.map((element: string, index: number) => (
-            <span key={index}>{element}</span>
-          ))}
+        <span>{error}</span>
       </div>
     );
   };
   return (
     <>
-      {/* <ErrorMessage name={name} component={Message} {...props} /> */}
-      {/* {errorMessage ? <Message error={errorMessage} /> : null} */}
+      <ErrorMessage name={name} component={Message} {...props} />
+      {errorMessage != null ? <Message error={errorMessage} /> : null}
     </>
   );
 };
--- src/pages/business/index.tsx
+++ src/pages/business/index.tsx
@@ -1,22 +1,33 @@
 import { useEffect, useState } from 'react';
+
 import type { NextPage } from 'next';
+
+import Card from '@components/Card';
+import Loader from '@components/Loader';
+import { ENTITYS } from '@components/data/Entitys';
+import { BusinessHeaders } from '@components/data/Headers';
 import { FlagSelector } from '@components/forms/FlagSelector';
+import Alert from '@components/layout/Alert';
 import Button from '@components/layout/Button';
 import MainContainer from '@components/layout/MainContainer';
+import SearchBar from '@components/layout/SearchBar';
 import { CreateForm } from '@components/pages/business/CreateForm';
-import { deleteBusiness, getBusiness, getBusinesses, postBusiness, putBusiness } from '@fetches/business';
+import MiTable from '@components/table/MiTable';
+
+import {
+  deleteBusiness,
+  getBusiness,
+  getBusinesses,
+  postBusiness,
+  putBusiness,
+} from '@fetches/business';
+
+import { flattenJSON } from '@utils/flattenJSON';
+
 import DomainAddIcon from '@mui/icons-material/DomainAdd';
 import { Box } from '@mui/system';
 import { FormikValues } from 'formik';
-import MiTable from '@components/table/MiTable';
-import SearchBar from '@components/layout/SearchBar';
 import useSWR from 'swr';
-import Loader from '@components/Loader';
-import { BusinessHeaders } from '@components/data/Headers';
-import { ENTITYS } from '@components/data/Entitys';
-import Card from '@components/Card';
-import Alert from '@components/layout/Alert';
-import { flattenJSON } from '@utils/flattenJSON';
 
 const BusinessButton = ({ onclick }: any) => {
   return (
@@ -28,38 +39,37 @@ const BusinessButton = ({ onclick }: any) => {
 
 let initValues = {
   client: {
-    phone_number: "+584241315948",
-    fav_course: "PHP",
-    notification_frecuency: "1 vez al mes",
-    offered_services: "lulu",
-    whatsapp: "+584241315946",
+    phone_number: '+584241315948',
+    fav_course: 'PHP',
+    notification_frecuency: '1 vez al mes',
+    offered_services: 'lulu',
+    whatsapp: '+584241315946',
     addresses: [
       {
-        line1: "lin1",
-        line2: "lin2",
-        country: "ve",
-        city: "Caracas",
-        state: "Distrito Capital",
-      }
+        line1: 'lin1',
+        line2: 'lin2',
+        country: '',
+        city: 'Caracas',
+        state: 'Distrito Capital',
+      },
     ],
     socials: [
       {
-        name: "string",
-        value: "string"
-      }
-    ]
+        name: 'string',
+        value: 'string',
+      },
+    ],
   },
-  name: "Pepa",
-  email: "gustariz@mahisoft.com",
-  services: "lili",
-  tax_id: "2222222",
-  website: "https://mui.com/material-ui/api/button/"
+  name: 'Pepa',
+  email: 'gustariz@mahisoft.com',
+  services: 'lili',
+  tax_id: '2222222',
+  website: 'https://mui.com/material-ui/api/button/',
 };
 // as BusinessForm
 
 const Business: NextPage = () => {
-
-  const [businessData, setBusinessData] = useState()
+  const [businessData, setBusinessData] = useState();
 
   const [openCreate, setOpenCreate] = useState(false);
 
@@ -78,7 +88,7 @@ const Business: NextPage = () => {
   const handleCloseCreate = () => {
     setOpenCreate(false);
     setEditable(false);
-    setInitial(initValues)
+    setInitial(initValues);
     mutate();
   };
 
@@ -92,27 +102,26 @@ const Business: NextPage = () => {
   };
 
   const handleEditRow = async (id: number) => {
-    setId(id)
+    setId(id);
     setEditable(true);
     try {
-      initValues = await getBusiness(id);;
+      initValues = await getBusiness(id);
     } catch (exception: any) {
       // setLoading(false);
     }
     // get de la variable y setear initial values
-    handleClickOpenCreate()
-  }
+    handleClickOpenCreate();
+  };
 
   const handleDeleteRow = (id: number) => {
     // console.log("He aqui el id", id)
-    setId(id)
-    handleClickOpenDelete()
-  }
-
+    setId(id);
+    handleClickOpenDelete();
+  };
 
   // const [loading, setLoading] = useState(false);
 
-  const { data: business, mutate} = useSWR('business', getBusinesses);
+  const { data: business, mutate } = useSWR('business', getBusinesses);
 
   useEffect(() => {
     if (business) {
@@ -121,11 +130,14 @@ const Business: NextPage = () => {
         return flattenJSON(element);
       });
       // console.log("holi", businessFlaten)
-      setBusinessData(businessFlaten)
+      setBusinessData(businessFlaten);
     }
   }, [business]);
 
-  const handleSubmitCreate = async (values: FormikValues, { setStatus }: any) => {
+  const handleSubmitCreate = async (
+    values: FormikValues,
+    { setStatus }: any
+  ) => {
     // console.log("OnSubmit():", values);
     try {
       await postBusiness(values);
@@ -133,22 +145,22 @@ const Business: NextPage = () => {
       handleCloseCreate();
     } catch (exception: any) {
       // console.log("exceptions:", exception)
-      setStatus(exception.data);
+      setStatus(exception.data.detail);
       // setLoading(false);
     }
   };
 
   const handleSubmitEdit = async (values: FormikValues, { setStatus }: any) => {
     try {
-      console.log("edit", values, currentId)
+      console.log('edit', values, currentId);
       await putBusiness(values, currentId);
       setStatus({});
       handleCloseCreate();
     } catch (exception: any) {
-      console.log("exceptions:", exception);
+      console.log('exceptions:', exception);
       setStatus(exception.data);
     }
-  }
+  };
 
   const handleSubmitDelete = async () => {
     try {
@@ -159,18 +171,18 @@ const Business: NextPage = () => {
       // setStatus(exception.data);
       // setLoading(false);
     }
-  }
+  };
 
   const handleSelectFlag = (e: any) => {
     // console.log("Se selecciono la bandera de:", e);
-  }
+  };
 
   const styles = {
     '& form': {
-      height: '100%'
+      height: '100%',
     },
-    getBusiness
-  }
+    getBusiness,
+  };
 
   const stylesCard = {
     height: 100,
@@ -183,21 +195,26 @@ const Business: NextPage = () => {
   return (
     <MainContainer>
       <Box sx={{ maxWidth: 500 }}>
-        <Card name={ENTITYS[1].name}
+        <Card
+          name={ENTITYS[1].name}
           icon={ENTITYS[1].icon}
           color={ENTITYS[1].color}
           description={ENTITYS[1].description}
           link={ENTITYS[1].link}
-          style={stylesCard} />
+          style={stylesCard}
+        />
       </Box>
       <Box display="flex" justifyContent="space-between" className="my-8">
         <BusinessButton onclick={handleClickOpenCreate} />
-        <Box className="w-1/2 gap-x-4" display="flex" alignItems="center" justifyContent="space-between">
+        <Box
+          className="w-1/2 gap-x-4"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
           <SearchBar />
           <Box className="w-1/2">
-            <FlagSelector
-              onSelect={handleSelectFlag}
-            ></FlagSelector>
+            <FlagSelector onSelect={handleSelectFlag}></FlagSelector>
           </Box>
         </Box>
       </Box>
@@ -206,15 +223,25 @@ const Business: NextPage = () => {
         handleClose={handleCloseCreate}
         handleSubmit={!editable ? handleSubmitCreate : handleSubmitEdit}
         initValues={initialValues}
-        edit={editable} />
-      {initialValues && currentId && <Alert open={openDelete}
-        handleClose={handleCloseDelete}
-        handleSubmit={handleSubmitDelete}>{`¿Está seguro que desea eliminar a ${currentId}?`}</Alert>}
-      {businessData ? <MiTable rows={businessData}
-        headTable={BusinessHeaders}
-        handleEditRow={handleEditRow}
-        handleDeleteRow={handleDeleteRow}></MiTable>
-        : <Loader />}
+        edit={editable}
+      />
+      {initialValues && currentId && (
+        <Alert
+          open={openDelete}
+          handleClose={handleCloseDelete}
+          handleSubmit={handleSubmitDelete}
+        >{`¿Está seguro que desea eliminar a ${currentId}?`}</Alert>
+      )}
+      {businessData ? (
+        <MiTable
+          rows={businessData}
+          headTable={BusinessHeaders}
+          handleEditRow={handleEditRow}
+          handleDeleteRow={handleDeleteRow}
+        ></MiTable>
+      ) : (
+        <Loader />
+      )}
     </MainContainer>
   );
 };
